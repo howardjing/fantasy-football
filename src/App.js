c639214bace85e5db26fad8e415810c52c2dec35
Howard Jing <howard.jing@gmail.com>
2018-08-28 23:32:53 -0400

    add ability to create player

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Select from 'react-select';
 import './App.css';
 import PLAYERS from './data';
 
-const PLAYERS_SET = new Set(PLAYERS);
-
 const getAllSelectedPlayers = (selectedPlayersByTeam) => {
   return Object.values(selectedPlayersByTeam).reduce((acc, val) => acc.concat(val), []);
 };
@@ -13,9 +11,9 @@ const getPlayersSelectedBy = (selectedPlayersByTeam, player) => {
   return selectedPlayersByTeam[player] || [];
 };
 
-const getRemainingPlayers = (selectedPlayersByTeam) => {
+const getRemainingPlayers = (allPlayers, selectedPlayersByTeam) => {
   const selected = getAllSelectedPlayers(selectedPlayersByTeam);
-  const remaining = new Set(PLAYERS_SET.values());
+  const remaining = new Set(allPlayers);
   selected.forEach(x => remaining.delete(x));
   return Array.from(remaining.values());
 }
@@ -38,29 +36,45 @@ const RenderPlayer = ({ player }) => {
 }
 
 class App extends Component {
+  createPlayerInput = React.createRef();
+
   state = {
-    numPlayers: 15,
+    numPickers: 15,
     turn: 0,
-    displayedPlayer: 3,
+    displayedPicker: 3,
+    allPlayers: PLAYERS,
     selectedPlayers: {
     },
   };
 
   changeDisplayedPlayer = (e) => {
-    const player = parseInt(e.currentTarget.value, 10);
-    console.log("NO", player)
+    const picker = parseInt(e.currentTarget.value, 10);
+    console.log("NO", picker)
     this.setState(() => ({
-      displayedPlayer: player,
+      displayedPicker: picker,
     }));
   };
 
+  addPlayer = (e) => {
+    e.preventDefault();
+    const playerName = this.createPlayerInput.current.value;
+    const { allPlayers } = this.state;
+    const player = {
+      name: playerName,
+      position: 'N/A',
+      team: 'N/A',
+      byeWeek: 'N/A',
+    }
+    this.setState(() => ({
+      allPlayers: allPlayers.concat([player])
+    }));
+  }
+
   draftPlayer = (option) => {
-    console.log("DD", option)
-    const { turn, numPlayers, selectedPlayers } = this.state;
-    const picker = getPicker(numPlayers, turn);
+    const { turn, numPickers, selectedPlayers } = this.state;
+    const picker = getPicker(numPickers, turn);
     const picked = getPlayersSelectedBy(selectedPlayers, picker);
     const updated = picked.concat([option.value]);
-    console.log("OKAY", updated)
     this.setState(() => ({
       turn: turn + 1,
       selectedPlayers: {
@@ -71,8 +85,8 @@ class App extends Component {
   }
 
   render() {
-    const { turn, selectedPlayers, displayedPlayer, numPlayers } = this.state;
-    const players = getRemainingPlayers(selectedPlayers);
+    const { turn, selectedPlayers, displayedPicker, allPlayers, numPickers } = this.state;
+    const players = getRemainingPlayers(allPlayers, selectedPlayers);
     return (
       <div style={{ display: 'flex' }}>
         <ol style={{ flex: 2 }}>
@@ -85,16 +99,21 @@ class App extends Component {
           })}
         </ol>
         <div style={{ flex: 1 }}>
-          <div>Turn: {turn}, Picker: {getPicker(numPlayers, turn)}, Teams: {numPlayers}</div>
+          <div>Turn: {turn}, Picker: {getPicker(numPickers, turn)}, Teams: {numPickers}</div>
           <div>
             <label>Choose player
                 <Select onChange={this.draftPlayer} options={players.map(x => ({ value: x, label: `${x.name}, ${x.position}, ${x.team}, ${x.byeWeek}`}))} />
             </label>
           </div>
           <div>
-            <label>Players selected by <input type="number" value={displayedPlayer} onChange={this.changeDisplayedPlayer} /></label>
+            <form onSubmit={this.addPlayer}>
+            <label>Create player <input type="text" ref={this.createPlayerInput} /></label>
+            </form>
+          </div>
+          <div>
+            <label>Players selected by <input type="number" value={displayedPicker} onChange={this.changeDisplayedPlayer} /></label>
             <ol>
-              {getPlayersSelectedBy(selectedPlayers, displayedPlayer).map(player => {
+              {getPlayersSelectedBy(selectedPlayers, displayedPicker).map(player => {
                 return (
                   <li key={`${player.name}-${player.team}`}>
                     <RenderPlayer player={player} />
